644696ce7a5ec47f58c43fd353fa3650ce5c19c4
Nauxe <exingulis@gmail.com>
2017-02-17 06:13:12 +0900

    Added QOL changes

--- buttcare.py
+++ buttcare.py
@@ -8,21 +8,23 @@ import validators
 from win10toast import ToastNotifier
 
 toaster = ToastNotifier()
-KEY = "bb53b5f779511dc0152ae3cd366714"
+KEY = "heyheresenpai"
 API = "http://easy.butt.care/api/v2/action/shorten"
 
 
 if __name__ == '__main__':
     last = ""
+    lastLink = "" #won't re-shorten the last unshortened link.
     while True:
         time.sleep(1)
-        url = clipboard.paste()
+        url = clipboard.paste() #gets clipboard data
 
-        if validators.url(url) and last != url:
+        if validators.url(url) and last != url and lastLink != url:
             print("Link Found: {0}".format(url))
             toSend = requests.get(API, {"key" : KEY, "url" : url})
 
             if validators.url(str(toSend.text)):
+                lastLink = url
                 last = str(toSend.text)
                 clipboard.copy(str(toSend.text))
                 toaster.show_toast("Butts", "Link converted to something more sexy.")
